c0e4c64df3cb379f2f346fe2ac93a85211d9f7a5
polaris <dev@afterlike.org>
2026-06-20 23:18:08 -0400

    refactor: strip annotations

--- src/main/java/org/afterlike/examplemod/ExampleMod.java
+++ src/main/java/org/afterlike/examplemod/ExampleMod.java
@@ -3,14 +3,12 @@ package org.afterlike.examplemod;
 import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import re.tsuku.fastbus.FastBus;
 
 public class ExampleMod {
 	private static final Logger LOGGER = LogManager.getLogger();
-	private static final @Nullable ExampleMod INSTANCE = new ExampleMod();
-	private static final @NotNull String VERSION = org.afterlike.examplemod.BuildConstants.VERSION;
+	private static final ExampleMod INSTANCE = new ExampleMod();
+	private static final String VERSION = org.afterlike.examplemod.BuildConstants.VERSION;
 	private final FastBus eventBus;
 	public ExampleMod() {
 		this.eventBus = new FastBus();
@@ -26,7 +24,7 @@ public class ExampleMod {
 		// late initialization logic
 	}
 
-	public static @NotNull ExampleMod get() {
+	public static ExampleMod get() {
 		return Objects.requireNonNull(INSTANCE);
 	}
 
@@ -34,7 +32,7 @@ public class ExampleMod {
 		return eventBus;
 	}
 
-	public @NotNull String getVersion() {
+	public String getVersion() {
 		return VERSION;
 	}
 }
